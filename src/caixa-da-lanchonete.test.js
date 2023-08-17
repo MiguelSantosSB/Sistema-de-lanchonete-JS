75d018e49ef7d8611543c2961d03efb970ed93cf
Miguel <miguel.santoss085@gmail.com>
2023-08-16 22:11:48 -0300

    testes de acordo com os blocos do arquivo

---
+++ src/caixa-da-lanchonete.test.js
@@ -0,0 +1,39 @@
+import { CaixaDaLanchonete } from "./caixa-da-lanchonete.js";
+
+let resultado = new CaixaDaLanchonete()
+
+// let bloco1e2 = false
+// let bloco3e4 = false
+// let bloco5e6 = false
+
+// if(bloco1e2===true){
+//     resultado.calculaValorDaCompra('dinheiro',[])
+//     resultado.calculaValorDaCompra('credito',[])
+//     resultado.calculaValorDaCompra('debito',[])    
+// }
+// if(bloco1e2===false){
+    // resultado.calculaValorDaCompra('dinheiro',['cafe,1'])
+    // resultado.calculaValorDaCompra('credito',['cafe,1'])
+    // resultado.calculaValorDaCompra('debito',['cafe,1']) 
+// }
+// if(bloco3e4===true){
+//     resultado.calculaValorDaCompra('credito',['cafe,1', 'sanduiche,1', 'queijo,1'])
+//     resultado.calculaValorDaCompra('debito',['cafe,1', 'sanduiche,1', 'queijo,1']) 
+// }
+// if(bloco3e4 === false){
+//     resultado.calculaValorDaCompra('dinheiro',['cafe,4', 'sanduiche,3', 'queijo,2'])
+//     resultado.calculaValorDaCompra('credito',['cafe,4', 'sanduiche,3', 'queijo,2'])
+//     resultado.calculaValorDaCompra('debito',['cafe,4', 'sanduiche,3', 'queijo,2']) 
+// }
+// if(bloco5e6 === true){
+    // resultado.calculaValorDaCompra('dinheiro',['cafe,0'])
+    // resultado.calculaValorDaCompra('credito',['4'])
+    // resultado.calculaValorDaCompra('debito',['pizza,1']) 
+    // resultado.calculaValorDaCompra('especie',['cafe,1']) 
+// }
+// if(bloco5e6 === false){
+    // resultado.calculaValorDaCompra('dinheiro',['chantily,1'])
+    // resultado.calculaValorDaCompra('credito',['queijo,1'])
+    // resultado.calculaValorDaCompra('credito',['chantily,1','sanduiche,1']) 
+    // resultado.calculaValorDaCompra('debito',['cafe,1','queijo,1']) 
+// }
